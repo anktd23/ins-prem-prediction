31d8ec0ab9b78d9ce8a07ab8110029c92ccd1426
Aniket Dumbre <anktd23@gmail.com>
2023-01-09 22:41:10 +0530

    .gitignore updated

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, render_template
-import pandas as pd
+from flask import Flask,render_template,request,redirect
+from flask_cors import CORS,cross_origin
 import pickle
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
-
+cors=CORS(app)
 
 file = open("/config/workspace/saved_models/2/model/model.pkl", 'rb')
 model = pickle.load(file)
 
 data = pd.read_csv('/config/workspace/insurance.csv')
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
     sex = sorted(data['sex'].unique())
     smoker = sorted(data['smoker'].unique())
@@ -18,6 +20,7 @@ def index():
     return render_template('index.html', sex= sex, smoker= smoker, region= region)
 
 @app.route('/predict', methods=['POST'])
+@cross_origin()
 def predict():
     age = int(request.form.get('age'))
     sex = request.form.get('sex')
@@ -26,8 +29,7 @@ def predict():
     smoker = request.form.get('smoker')
     region = request.form.get('region')
 
-    prediction = model.predict(pd.DataFrame([[age, sex, bmi, children, smoker, region]], 
-                columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region']))
+    prediction = model.predict(pd.DataFrame(columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region'],data=np.array(['age', 'sex', 'bmi', 'children', 'smoker', 'region']).reshape(1, 6)))
 
     return str(prediction[0])           
 
